c2ffa4043f1953e7104c4036712dcd299e3273e3
Dais <daispaysagist@gmail.com>
2019-01-21 16:27:25 +0200

    finished ID3 and perceptron experiments.

--- classifier.py
+++ classifier.py
@@ -3,6 +3,7 @@ import numpy as np
 import operator
 import csv
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import Perceptron
 
 
 class knn_classifier(abstract_classifier):
@@ -141,10 +142,17 @@ def calculate_evaluation_accuracy_error(data, factory) -> (float, float):
                     np.vstack((training_set[0], data[j][0]))
                     np.vstack((training_set[1], data[j][1]))
         classifier = factory.train(training_set[0], training_set[1])
-        for example, n in zip(test_set[0], range(len(test_set[0]))):
-            experiment_count += 1
-            if classifier.classify(example) != test_set[1][n]:
-                error_count += 1
+        if factory is knn_factory:
+            for example, n in zip(test_set[0], range(len(test_set[0]))):
+                experiment_count += 1
+                if classifier.classify(example) != test_set[1][n]:
+                    error_count += 1
+        else:
+            classifier_labels = classifier.classify(test_set[0])
+            for label, n in zip(classifier_labels, range(len(classifier_labels))):
+                experiment_count += 1
+                if label != test_set[1][n]:
+                    error_count += 1
     avg_error = error_count / experiment_count
     return 1 - avg_error, avg_error
 
@@ -157,18 +165,17 @@ def evaluate(classifier_factory: abstract_classifier_factory, k: int) -> (float,
     # data is now a dictionary of the form {i:(features, labels)} where i is the key
     # and (features, labels) is the value, which is a tuple of the features matrix
     # and the labels vector
-
     return calculate_evaluation_accuracy_error(data, classifier_factory)
 
 
-def export_to_csv(results):
-    with open('experiments6.csv', 'w', newline='') as file:
+def export_to_csv(results, filename):
+    with open(filename, 'w', newline='') as file:
         wr = csv.writer(file, quoting=csv.QUOTE_ALL)
         for row in results:
             wr.writerow(list(row))
 
 
-def Question5():
+def question5():
     k_list = [1, 3, 5, 7, 13]
     results = []
     for k in k_list:
@@ -176,14 +183,64 @@ def Question5():
         factory = knn_factory(k)
         accuracy, error = evaluate(factory, 2)
         results.append((k, accuracy, error))
-    export_to_csv(results)
+    export_to_csv(results, 'experiments6.csv')
 
 
-class ID3Classifier(DecisionTreeClassifier):
+class ID3_classifier:
+    def __init__(self, data: np.ndarray, labels: np.ndarray):
+        """
+        This constructor also trains the classifier by creating the decision tree.
+        :param data: the unlabeled data of the dataset
+        :param labels: the correct labels of the data
+        """
+
+        self.inner_classifier = DecisionTreeClassifier(criterion="entropy")
+        self.inner_classifier.fit(data, labels)
+
+    def classify(self, features) -> int:
+        return self.inner_classifier.predict(features)
 
 
-def Question7():
-    classifier = DecisionTreeClassifier(criterion="entropy")
+class ID3_factory(abstract_classifier_factory):
+    def train(self, data, labels) -> ID3_classifier:
+        return ID3_classifier(data, labels)
+
+
+class perceptron_classifier:
+    def __init__(self, data: np.ndarray, labels: np.ndarray):
+        """
+        This constructor also trains the classifier by creating the decision tree.
+        :param data: the unlabeled data of the dataset
+        :param labels: the correct labels of the data
+        """
+
+        self.inner_classifier = Perceptron(tol=1e-3, random_state=0)
+        self.inner_classifier.fit(data, labels)
+
+    def classify(self, features) -> int:
+        return self.inner_classifier.predict(features)
+
+
+class perceptron_factory(abstract_classifier_factory):
+    def train(self, data, labels) -> perceptron_classifier:
+        return perceptron_classifier(data, labels)
+
+
+def question7():
+    # ID3
+    id3_results = []
+    factory = ID3_factory()
+    accuracy, error = evaluate(factory, 2)
+    id3_results.append((1, accuracy, error))
+    export_to_csv(id3_results, 'experiments12ID3.csv')
+
+    # Perceptron
+    perceptron_results = []
+    factory = perceptron_factory()
+    accuracy, error = evaluate(factory, 2)
+    perceptron_results.append((2, accuracy, error))
+    export_to_csv(perceptron_results, 'experiments12perceptron.csv')
+
 
 def main():
     # dataset is a 3-tuple consisting of:
@@ -199,8 +256,8 @@ def main():
     # load_k_fold_data(2)
     # patients, labels, test = load_data()
     # split_crosscheck_groups(patients, labels, 2)
-    # Question5()
-    Question7()
+    # question5()
+    question7()
 
 
 if __name__ == '__main__':
